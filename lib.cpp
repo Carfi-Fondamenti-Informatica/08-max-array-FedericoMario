4a61d096487985fd8c38f809374dab292dae58a3
FedericoMario <117830052+FedericoMario@users.noreply.github.com>
2022-12-13 10:13:09 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include "lib.h"
 
+float maxArray (float num[] , int d) {
+    int i=0;
+        while (i < d) {
+        if (num[0] > num[i]) {
+            i++;
+        }else {
+            num[0] = num[i];
+            i++; }
+        }
+    return num[0];
+}
